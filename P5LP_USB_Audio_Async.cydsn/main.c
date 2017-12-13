6638747ef288423b7f660678f64e67a6fddb1c80
noeldiviney <noel@ayrenet.com>
2017-12-14 08:12:27 +1100

    updating

---
+++ P5LP_USB_Audio_Async.cydsn/main.c
@@ -0,0 +1,29 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+
+int main(void)
+{
+//    P5LP_059_Dbg_init();
+
+//    CyDelay(10u);
+    TriggerScope();
+    CyDelay(2u);
+    LED_1_Write(1u);
+    P5LP_059_Dbg_init();
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
